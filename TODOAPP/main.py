ad6a410600fceb8d016b056b56dd92a5cca68f22
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-02-04 12:14:40 +0530

    Get Todo by Id

--- TODOAPP/main.py
+++ TODOAPP/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
 from models import Todos
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from starlette import status
 
 
 app = FastAPI()
@@ -22,6 +23,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo_by_id(db: db_dependency, todo_id: int = Path(..., gt=0)):
+    todos_list = db.query(Todos).filter(Todos.id == todo_id).first()
+    print(todos_list)
+    if todos_list is not None:
+        return todos_list
+    else:
+        raise HTTPException(status_code=404, detail="Todo not found")
