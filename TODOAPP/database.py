0d51a0bedf0bb801c08fdc9f62419adefd2b0be0
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-02-05 14:56:43 +0530

    Postgresql integration

--- TODOAPP/database.py
+++ TODOAPP/database.py
@@ -1,13 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# SQLITE3
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# POSTGRESQL DATABASE
 
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- TODOAPP/main.py
+++ TODOAPP/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-import models
-from database import engine
-from routers import auth, todo, admin
+
+
+# import models
+from database import engine, Base
+from routers import auth, todo, admin, user
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 app.include_router(auth.router)
 app.include_router(todo.router)
 app.include_router(admin.router)
+app.include_router(user.router)
