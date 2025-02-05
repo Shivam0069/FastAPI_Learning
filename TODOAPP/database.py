f787c79673a6b36805851000212fcf178d1d3a36
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-02-05 16:09:53 +0530

    MYSQL Integration

--- TODOAPP/database.py
+++ TODOAPP/database.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("MYSQL_DATABASE_URL")
 # POSTGRESQL DATABASE
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
